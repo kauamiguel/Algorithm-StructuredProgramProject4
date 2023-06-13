4954fb45fc814c95928c5efe5b45136dc256e4db
kauamiguel <kauamcm@gmail.com>
2023-06-13 12:33:14 -0300

    first Version of project

---
+++ projetoFinal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define R 25
+
+float calcularPeso(float V){
+    return V*R;
+}
+
+int main(){
+    //Variaveis do volume
+    float altura, largura, comprimento, volumeTotal;
+    float pesoGuindaste;
+
+    printf("Digite o valor da altura: \n");
+    scanf("%f",altura);
+    printf("Digite o valor da largura: \n");
+    scanf("%f",largura);
+    printf("Digite o valor da comprimento: \n");
+    scanf("%f",comprimento);
+    volumeTotal = altura * comprimento * largura;
+
+    pesoGuindaste = calcularPeso(volumeTotal);
+
+}
