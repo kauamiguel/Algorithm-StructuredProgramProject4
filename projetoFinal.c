dac0f484503ff99f63b3e93b27aa4ad52019d626
Joel-ASantos <116813314+Joel-ASantos@users.noreply.github.com>
2023-06-17 11:43:59 -0300

    Update projetoFinal.c

--- projetoFinal.c
+++ projetoFinal.c
@@ -13,65 +13,65 @@ int main()
     system("color 02");
 
     do{
-        printf("Digite 1 para acessar o programa e 2 para encerrar\n");
-        printf("Escolha: ");
-        scanf("%d", &escolha);
-
+            printf("Digite 1 para acessar o programa e 2 para encerrar\n");
+            printf("Escolha: ");
+            scanf("%d", &escolha);
+    
         switch (escolha)
-    {
-        case 1:
-            system("cls");
-
-            // Variaveis do volume
-            float altura, largura, comprimento, volumeTotal;
-            float pesoGuindaste;
-
-            system("color 02");
-
-            // pegando as informações do usuario
-            printf("Digite o valor da altura: \n");
-            scanf("%f", &altura);
-            printf("Digite o valor da largura: \n");
-            scanf("%f", &largura);
-            printf("Digite o valor da comprimento: \n");
-            scanf("%f", &comprimento);
-
-            // Calculando o peso do guindaste
-            volumeTotal = altura * comprimento * largura;
-            pesoGuindaste = calcularPeso(volumeTotal);
-
-            // G1 suporta 2000 kg; G2 suporta 4000 kg e G3 8000 KG
-        
-            if (volumeTotal <= 2000)
-            {
-                printf("Guindaste a ser utilizado: G1\n");
-            }
-            else if (volumeTotal > 2000 && volumeTotal <= 4000)
-            {
-                printf("Guindaste a ser utilizado: G2\n");
-            }
-            else if (volumeTotal > 4000 && volumeTotal <= 8000)
-            {
-                printf("Guindaste a ser utilizado: G3\n");
-            }
-            else
-            {
-                printf("Não foi possivel localizar o guindaste a ser usado");
-            }
-
-            exit(0);
-        break;
-
-        case 2:
-            printf("Encerrando o programa\n");
-            exit(0);
-        break;
-
-        default:
-            printf("Opcao invalida!\n");
-            system("pause\n");
-            system("cls");
-        break;
-    }
+        {
+            case 1:
+                system("cls");
+    
+                // Variaveis do volume
+                float altura, largura, comprimento, volumeTotal;
+                float pesoGuindaste;
+    
+                system("color 02");
+    
+                // pegando as informações do usuario
+                printf("Digite o valor da altura: \n");
+                scanf("%f", &altura);
+                printf("Digite o valor da largura: \n");
+                scanf("%f", &largura);
+                printf("Digite o valor da comprimento: \n");
+                scanf("%f", &comprimento);
+    
+                // Calculando o peso do guindaste
+                volumeTotal = altura * comprimento * largura;
+                pesoGuindaste = calcularPeso(volumeTotal);
+    
+                // G1 suporta 2000 kg; G2 suporta 4000 kg e G3 8000 KG
+            
+                if (volumeTotal <= 2000)
+                {
+                    printf("Guindaste a ser utilizado: G1\n");
+                }
+                else if (volumeTotal > 2000 && volumeTotal <= 4000)
+                {
+                    printf("Guindaste a ser utilizado: G2\n");
+                }
+                else if (volumeTotal > 4000 && volumeTotal <= 8000)
+                {
+                    printf("Guindaste a ser utilizado: G3\n");
+                }
+                else
+                {
+                    printf("Não foi possivel localizar o guindaste a ser usado");
+                }
+    
+                exit(0);
+            break;
+    
+            case 2:
+                printf("Encerrando o programa\n");
+                exit(0);
+            break;
+    
+            default:
+                printf("Opcao invalida!\n");
+                system("pause\n");
+                system("cls");
+            break;
+        }
     }while(escolha != 1 || escolha != 2);
 }
