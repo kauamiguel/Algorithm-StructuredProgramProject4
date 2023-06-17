7f2cf8606827d992f145da588a6ed1a23eb8f849
kauamiguel <kauamcm@gmail.com>
2023-06-17 10:26:36 -0300

    Adding background

--- projetoFinal.c
+++ projetoFinal.c
@@ -10,14 +10,18 @@ int main(){
     float altura, largura, comprimento, volumeTotal;
     float pesoGuindaste;
 
+    system("02");
+
+    //pegando as informações do usuario
     printf("Digite o valor da altura: \n");
     scanf("%f",&altura);
     printf("Digite o valor da largura: \n");
     scanf("%f",&largura);
     printf("Digite o valor da comprimento: \n");
     scanf("%f",&comprimento);
-    volumeTotal = altura * comprimento * largura;
 
+    //Calculando o peso do guindaste
+    volumeTotal = altura * comprimento * largura;
     pesoGuindaste = calcularPeso(volumeTotal);
 
     
