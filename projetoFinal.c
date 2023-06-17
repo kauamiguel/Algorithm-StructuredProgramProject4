e62c25df4bed7a174f10653d510243eaba82e151
Ítalo Santana <112109465+italoeugenio@users.noreply.github.com>
2023-06-17 11:33:49 -0300

    Update projetoFinal.c

--- projetoFinal.c
+++ projetoFinal.c
@@ -2,53 +2,63 @@
 #include <stdlib.h>
 #define R 25
 
-float calcularPeso(float V){
-    return V*R;
+float calcularPeso(float V)
+{
+    return V * R;
 }
 
-int main(){
+int main()
+{
     int escolha;
 
     printf("Digite 1 para acessar o programa e 2 para encerrar\n");
     printf("Escolha: ");
-    scanf("%d",&escolha);
+    scanf("%d", &escolha);
 
-    switch(escolha){
-        case 1:
-            system("cls");
+    switch (escolha)
+    {
+    case 1:
+        system("cls");
 
-            //Variaveis do volume
-            float altura, largura, comprimento, volumeTotal;
-            float pesoGuindaste;
+        // Variaveis do volume
+        float altura, largura, comprimento, volumeTotal;
+        float pesoGuindaste;
 
-            system("color 02");
+        system("color 02");
 
-            //pegando as informações do usuario
-            printf("Digite o valor da altura: \n");
-            scanf("%f",&altura);
-            printf("Digite o valor da largura: \n");
-            scanf("%f",&largura);
-            printf("Digite o valor da comprimento: \n");
-            scanf("%f",&comprimento);
+        // pegando as informações do usuario
+        printf("Digite o valor da altura: \n");
+        scanf("%f", &altura);
+        printf("Digite o valor da largura: \n");
+        scanf("%f", &largura);
+        printf("Digite o valor da comprimento: \n");
+        scanf("%f", &comprimento);
 
-            //Calculando o peso do guindaste
-            volumeTotal = altura * comprimento * largura;
-            pesoGuindaste = calcularPeso(volumeTotal);
+        // Calculando o peso do guindaste
+        volumeTotal = altura * comprimento * largura;
+        pesoGuindaste = calcularPeso(volumeTotal);
 
-            if (volumeTotal <= 2000) {
-        printf("Guindaste a ser utilizado: G1\n");
-    } else if (volumeTotal > 2000 && volumeTotal <= 4000) {
-        printf("Guindaste a ser utilizado: G2\n");
-    } else if  (volumeTotal > 4000 && volumeTotal <= 8000){
-        printf("Guindaste a ser utilizado: G3\n");
-    } else {
-        printf("Não foi possivel localizar o guindaste a ser usado");
-            }
-            //G1 suporta 2000 kg; G2 suporta 4000 kg e G3 8000 KG
+        if (volumeTotal <= 2000)
+        {
+            printf("Guindaste a ser utilizado: G1\n");
+        }
+        else if (volumeTotal > 2000 && volumeTotal <= 4000)
+        {
+            printf("Guindaste a ser utilizado: G2\n");
+        }
+        else if (volumeTotal > 4000 && volumeTotal <= 8000)
+        {
+            printf("Guindaste a ser utilizado: G3\n");
+        }
+        else
+        {
+            printf("Não foi possivel localizar o guindaste a ser usado");
+        }
+        // G1 suporta 2000 kg; G2 suporta 4000 kg e G3 8000 KG
         break;
 
-        case 2:
-            exit(0);
+    case 2:
+        exit(0);
         break;
     }
 }
